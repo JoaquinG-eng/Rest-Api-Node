6cf398dc3a7e42a07abcdb781bb9d75b3b3994e1
Joaquin <joamengancho@gmail.com>
2026-05-11 17:56:05 -0300

    fix: ruta absoluta para swagger yaml en produccion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 import authorsRoutes from "./routes/Authors.Routes.js";
 import postsRoutes from "./routes/Posts.Routes.js";
 import { errorHandler } from "./middlewares/Error.Middlewares.js";
-import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -15,23 +20,20 @@ app.use(cors());
 app.use("/api/authors", authorsRoutes);
 app.use("/api/posts", postsRoutes);
 
-const swaggerDocument = YAML.load("src/yaml/swagger.yaml");
+const swaggerDocument = YAML.load(join(__dirname, "yaml/swagger.yaml"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
-
+ 
 app.get("/", (req, res) => {
 res.json({
-status: "200",
-endpoints: {
-authors: "/api/authors",
-posts: "/api/posts",
-docs: "/api-docs"
-}
-});
-});
-
-
+    status: "200",
+    endpoints: {
+      authors: "/api/authors",
+      posts: "/api/posts",
+      docs: "/api-docs"
+    }
+  });
+ });
+ 
 app.use(errorHandler);
 
 export default app;
