ca6a0a67ba3a3da094f9d0933174dff8acb7c145
Joaquin <joamengancho@gmail.com>
2026-05-20 19:26:17 -0300

    correcion de URLs

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:5173",
-    "https://tu-proyecto.vercel.app"  // ← reemplazá con tu URL de Vercel cuando la tengas
+    "https://rest-api-node-pbza.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
