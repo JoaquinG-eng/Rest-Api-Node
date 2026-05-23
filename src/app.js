21f83f6071ff00eddc4a4402efd3736d1487ea89
Joaquin <joamengancho@gmail.com>
2026-05-23 16:20:36 -0300

    add vercel url to cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:5173",
-    "https://rest-api-node-pbza.onrender.com"
+    "https://blog-frontend-rfa1.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
